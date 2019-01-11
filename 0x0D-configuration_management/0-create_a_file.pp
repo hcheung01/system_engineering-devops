a14a523b35bf97d05740f80fb851290b51ef292c
Heindrick Cheung <hcheung01@gmail.com>
2019-01-11 01:21:16 +0000

    checked lint

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # A resource declaration
 file { '/tmp/holberton':
-  ensure => file,
-  path   => '/tmp/holberton',
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
