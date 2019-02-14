b6b52bdbb04bbf99cb25c0f25770e6c64b9baa20
Heindrick Cheung <hcheung01@gmail.com>
2019-02-14 06:54:03 +0000

    refactored again

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,12 +13,12 @@ def recurse(subreddit, hot_list=[]):
     """recursively grab subreddit"""
 
     url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
-    r = s.get(url).json()
+    r = s.get(url).json()['data']
     try:
-        for t in r['data']['children']:
+        for t in r['children']:
             hot_list.append(t['data']['title'])
-        if r['data']['after']:
-            s.params = {'after': r['data']['after']}
+        if r['after']:
+            s.params = {'after': r['after']}
             return recurse(subreddit, hot_list)
         return hot_list
     except:
